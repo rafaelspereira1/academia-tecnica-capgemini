5c02fd3eb9510d4a3ad005a8900c7495efdf470f
Rafael Pereira <rafaelspereira1@hotmail.com>
2022-02-18 11:36:28 -0300

    Create solution for question 01

---
+++ questao-01.js
@@ -0,0 +1,14 @@
+let n = 5; // Altere o valor de N para a quantidade de * desejados
+let string = "";
+for (let i = 1; i <= n; i++) {
+  // printa o espaço em branco
+  for (let j = 0; j < n - i; j++) {
+    string += " ";
+  }
+  // printa o *
+  for (let k = 0; k < i; k++) {
+    string += "*";
+  }
+  string += "\n";
+}
+console.log(string);
